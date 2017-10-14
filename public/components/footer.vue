26fbaaca8b54cc126aec99571b1f4a14ce88c467
Tvolex <tisyanchin13@gmail.com>
2017-10-14 12:37:44 +0300

    footer

--- public/components/footer.vue
+++ public/components/footer.vue
@@ -1,12 +1,23 @@
 <template>
-    <v-parallax src="imgs/footer.jpg" height="90"></v-parallax>
+    <v-parallax src="imgs/footer.jpg" height="90">
+        <v-container fluid>
+            <v-layout row wrap>
+                <v-flex xs6 offset-xs3>
+                    <div align="center">Copyright {{currentYear}} <span style="color: blue">APT</span>| All Rights Reserved</div>
+                </v-flex>
+            </v-layout>
+        </v-container>
+
+    </v-parallax>
 </template>
 
 <script>
     export default {
         name: 'footer',
         data() {
-            return {}
+            return {
+                currentYear: new Date().getFullYear()
+            }
         }
     }
 </script>
